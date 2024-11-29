7083bca8565b0340c91c17e4c4afd88fb23fb6bc
Bhavya <bhavyaky0690@gmail.com>
2024-11-29 12:16:48 +0530

    Bring the wafers Data.

---
+++ src/constant/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+
+AWS_S3_BUCKET_NAME = "sensor-fault"
+MONGO_DATABASE_NAME = "bhavyak."
+MONGO_COLLECTION_NAME = "Sensorfault"
+
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL="mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder =  "artifacts"
